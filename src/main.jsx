e85941ffc3117967808bc4923ebef7ddbbb7b3c4
kikson9 <tochukwuokike7@gmail.com>
2026-08-31 11:45:57 +0800

    chore: install and wire up shadcn Tooltip provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -30,7 +31,9 @@ enableMocking().then(() => {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <App />
+          <TooltipProvider>
+            <App />
+          </TooltipProvider>
         </BrowserRouter>
       </QueryClientProvider>
     </StrictMode>,
